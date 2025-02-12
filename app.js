cbd59b58ec57afab2ff17c174ae5c1beee75e34f
jettpbaker <jett.p.baker@gmail.com>
2025-02-12 11:43:08 +1000

    Init

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello, World!</h1>`)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
