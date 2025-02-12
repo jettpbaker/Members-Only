49af3b5391a339258fc55e37489d774cd6e9bf2a
jettpbaker <jett.p.baker@gmail.com>
2025-02-12 12:20:27 +1000

    Created seed code for initial table setup in database

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+-- DB is created manually, this code is just for reference
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(255) NOT NULL,
+    salt TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    member BOOLEAN DEFAULT FALSE,
+    admin BOOLEAN DEFAULT FALSE
+);
