2d415165d1f50e65dd0203dc6e94b5d1349c45c5
jettpbaker <jett.p.baker@gmail.com>
2025-02-24 12:41:00 +1000

    Began setting up passport to work in the app

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 import express from 'express'
+import session from 'express-session'
+import connectPgSimple from 'connect-pg-simple'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import indexRouter from './routes/index.js'
+import router from './routes/index.js'
+import connection from './db/pool.js'
+import passport from 'passport'
+
+const PgStore = connectPgSimple(session)
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
 
-// View engine setup
+// View engine setup 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // Middleware
+
+const sessionStore = new PgStore({
+  pool: connection,
+  createTableIfMissing: true,
+})
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+  },
+}))
+
+app.use(passport.session())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
-app.use('/', indexRouter)
+app.use('/', router) 
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
