28bd034d869119ebf81f88054bb5443f3423661a
jettpbaker <jett.p.baker@gmail.com>
2025-02-25 16:48:41 +1000

    Added delete message icon

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,10 +3,12 @@ import { getMessages } from '../db/queries.js'
 export const getHome = async (req, res) => {
     const isAuthenticated = req.isAuthenticated()
     const isMember = req.user.member
+    const isAdmin = req.user.admin
     const messages = await getMessages()
     res.render('home', {
         isAuthenticated,
         isMember,
+        isAdmin,
         messages
     })
 } 
