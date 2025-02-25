92c1f6fd2639d1094d816027a5f53fc445a49fc3
jettpbaker <jett.p.baker@gmail.com>
2025-02-25 17:25:02 +1000

    Admin privileges set up, along with UI for it.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,8 +2,8 @@ import { getMessages } from '../db/queries.js'
 
 export const getHome = async (req, res) => {
     const isAuthenticated = req.isAuthenticated()
-    const isMember = req.user.member
-    const isAdmin = req.user.admin
+    const isMember = req.user ? req.user.member : false
+    const isAdmin = req.user ? req.user.admin : false
     const messages = await getMessages()
     res.render('home', {
         isAuthenticated,
