40f720dc6b2e8822e667dc3c12fd54e6affda415
jettpbaker <jett.p.baker@gmail.com>
2025-02-25 13:18:53 +1000

    New message component added and styled

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,6 @@
 export const getHome = (req, res) => {
     const isAuthenticated = req.isAuthenticated()
     res.render('home', {
-        title: 'Home Page',
-        message: 'Welcome to the home page',
         isAuthenticated
     })
 } 
